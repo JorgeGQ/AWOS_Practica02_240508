8fd6f6d6eb02be40b6a6a1f75bf72cb25a61f9c0
Jorge Olaf García Quiroga <jorgeolafgarciaquiroga@gmail.com>
2026-01-07 09:48:01 -0600

    Practica 02
    add: Se agrego el archivo del ejerccio 01 - Declaración de Variable aún sin terminar

---
+++ js/ejercicio01.js
@@ -0,0 +1,14 @@
+//Declaración de Variables
+var miNombre = "Jorge O.,";
+var misApellidos;
+//Utilizaremos la función console.log para imprimir el valor actual de la variable
+console.log("El valor de la variable miNombre es = ", miNombre);
+console.log("El valor de la variable misApellidos es = ", misApellidos);
+//Esto arroja un undefined dado que la variable  njo ha sido iniciualiazada , es decir no se le ha asignado un valor que almacenar
+
+misApellidos = "García Quiroga";
+//En este momento ña vvariable ya tiene un valor almacenado por que añlimprimirlo deberá mostrar dicho dato
+console.log("El valor de la variable misApellidos es = ", misApellidos);
+
+miNombre = "Jorge Olaf";
+console.log("Hola mi nombre completo es ", miNombre, " ", misApellidos)
